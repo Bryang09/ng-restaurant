670e1a5e1baf389b122dd2b31650e7de8d8457a4
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-11-28 10:18:52 -0600

    Project Initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LandingComponent } from './landing/landing.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { CateringComponent } from './catering/catering.component';
+import { HeroComponent } from './landing/hero/hero.component';
+import { Sub1Component } from './landing/sub1/sub1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingComponent,
+    MenuComponent,
+    AboutComponent,
+    CateringComponent,
+    HeroComponent,
+    Sub1Component
   ],
   imports: [
     BrowserModule,
